3b2ba9191afdada93dcc3b1c4f68068a7fe56f59
Gulshan Kay <42151394+gulkay@users.noreply.github.com>
2020-08-11 21:33:51 +0530

    changed directories

--- scripts/download_gutenburg_data.sh
+++ scripts/download_gutenburg_data.sh
@@ -4,8 +4,8 @@ NAME=$2
 
 URL=http://www.gutenberg.org/cache/epub/$FILE/pg$FILE.txt
 
-TARGET_DIR=./data/$NAME/
+TARGET_DIR=generative/data/$NAME/
 mkdir $TARGET_DIR
-TXT_FILE=./data/$NAME/data.txt
+TXT_FILE=generative/data/$NAME/data.txt
 wget -N $URL -O $TXT_FILE
 
